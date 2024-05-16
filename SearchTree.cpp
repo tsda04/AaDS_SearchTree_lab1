397e382d2fbc472362642dd9fa9ca2f546b754a0
tsda04 <tsda04@mail.ru>
2024-05-17 01:23:30 +0400

    + функции вывода и вставки элемента по значению

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -14,13 +14,48 @@ private:
         }
     };
     Node* root;
+
+    bool insertRecursive(Node*& node, int key) {
+        if (!node) {
+            node = new Node(key);
+            return true;
+        }
+        if (key < node->key) {
+            return insertRecursive(node->left, key);
+        }
+        else if (key > node->key) {
+            return insertRecursive(node->right, key);
+        }
+        return false; // элемент уже существует
+    }
+    void printRecursive(Node* node) const {
+        if (node) {
+            printRecursive(node->left);
+            std::cout << node->key << " ";
+            printRecursive(node->right);
+        }
+    }
 public:
     BinarySearchTree() : root(nullptr) {}
     ~BinarySearchTree() {
         delete root;
     }
+    bool insert(int key) {
+        return insertRecursive(root, key);
+    }
+    void print() const {
+        printRecursive(root);
+        std::cout << std::endl;
+    }
 };
 
 int main() {
+    BinarySearchTree tree;
+
+    tree.insert(5);
+    tree.insert(3);
+    tree.insert(7);
+
+    tree.print(); // Вывод: 3 5 7
     return 0;
 }
