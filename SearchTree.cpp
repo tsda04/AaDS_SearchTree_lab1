28a672f32a53a7c5660b06d98db878ab0fbc62ee
tsda04 <tsda04@mail.ru>
2024-05-17 00:53:13 +0400

    Создание класса

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
 
+class BinarySearchTree {
+private:
+    struct Node {
+        int key;
+        Node* left;
+        Node* right;
+
+        Node(int key) : key(key), left(nullptr), right(nullptr) {}
+        ~Node() {
+            delete left;
+            delete right;
+        }
+    };
+    Node* root;
+public:
+    BinarySearchTree() : root(nullptr) {}
+    ~BinarySearchTree() {
+        delete root;
+    }
+};
+
 int main() {
-	return 0;
+    return 0;
 }
